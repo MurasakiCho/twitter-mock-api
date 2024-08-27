90f00d16c472393922d3c31526f98ac4180db328
hicksjx2397 <josephdalehicks@outlook.com>
2024-08-27 13:18:36 -0400

    Controllers done mappers started

---
+++ src/main/java/controllers/TagController.java
@@ -0,0 +1,18 @@
+package controllers;
+
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/tags")
+public class TagController {
+	
+//	@GetMapping
+//	
+//	@GetMapping("/tags/{label}")
+
+}
