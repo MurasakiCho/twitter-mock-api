d7bef66bb97806d2df362c767798ce080ac91f57
Sylrelessa <briana.oneal823@gmail.com>
2024-08-27 16:36:06 -0400

    added service interfaces and impls

--- src/main/java/com/cooksystems/GroupProject1/mappers/TweetMapper.java
+++
@@ -1,8 +0,0 @@
-package com.cooksystems.GroupProject1.mappers;
-
-import org.mapstruct.Mapper;
-
-@Mapper(componentModel = "spring", uses = { HashtagMapper.class } )
-public interface TweetMapper {
-
-}
