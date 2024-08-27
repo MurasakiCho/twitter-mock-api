4e9727c74aedce9bfe3ee60fd3c417df7e48318f
Sylrelessa <briana.oneal823@gmail.com>
2024-08-27 12:20:12 -0400

    developed all dtos

---
+++ src/main/java/com/cooksystems/GroupProject1/dtos/UserResponseDto.java
@@ -0,0 +1,14 @@
+package com.cooksystems.GroupProject1.dtos;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.sql.Timestamp;
+
+@NoArgsConstructor
+@Data
+public class UserResponseDto {
+    private String username;
+    private ProfileDto profileDto;
+    private Timestamp timestamp;
+}
