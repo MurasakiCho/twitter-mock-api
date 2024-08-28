d8bfd0bdb6b38651c3a8f62db7c6a2d5ed1ed358
Philip T <philip1fox1@gmail.com>
2024-08-28 16:11:38 -0700

    Get tweets/{id} working

--- src/main/java/com/cooksystems/GroupProject1/controllers/TweetController.java
+++ src/main/java/com/cooksystems/GroupProject1/controllers/TweetController.java
@@ -1,5 +1,9 @@
 package com.cooksystems.GroupProject1.controllers;
 
+import com.cooksystems.GroupProject1.dtos.TweetResponseDto;
+import com.cooksystems.GroupProject1.services.TweetService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,10 +13,15 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 @RequestMapping("/tweets")
 public class TweetController {
+
+    private final TweetService tweetService;
 	
 //	@GetMapping
 //	
-//	@GetMapping("/{id}")
+	@GetMapping("/{id}")
+    public TweetResponseDto getTweetByID(@PathVariable Long id) {
+        return tweetService.getTweetByID(id);
+    }
 //	
 //	@GetMapping("/{id}/tags")
 //	
--- src/main/java/com/cooksystems/GroupProject1/services/Impl/TweetServiceImpl.java
+++ src/main/java/com/cooksystems/GroupProject1/services/Impl/TweetServiceImpl.java
@@ -1,10 +1,39 @@
 package com.cooksystems.GroupProject1.services.Impl;
 
+import com.cooksystems.GroupProject1.dtos.TweetResponseDto;
+import com.cooksystems.GroupProject1.entities.Tweet;
+import com.cooksystems.GroupProject1.exceptions.NotFoundException;
+import com.cooksystems.GroupProject1.mappers.TweetMapper;
+import com.cooksystems.GroupProject1.repositories.TweetRepository;
+import com.cooksystems.GroupProject1.services.TweetService;
 import org.springframework.stereotype.Service;
 
 import lombok.RequiredArgsConstructor;
 
+import java.util.Optional;
+
 @Service
 @RequiredArgsConstructor
-public class TweetServiceImpl {
+public class TweetServiceImpl implements TweetService {
+
+    private final TweetRepository tweetRepository;
+    private final TweetMapper tweetMapper;
+
+    @Override
+    public TweetResponseDto getTweetByID(Long id) {
+        Optional<Tweet> optionalTweet = tweetRepository.findById(id);
+        Tweet tweet;
+        //checking if tweet exist in database
+        if (optionalTweet.isPresent()) {
+            tweet = optionalTweet.get();
+        } else {
+            throw new NotFoundException("No Tweet found with id: " + id);
+        }
+        //checking if that tweet has been deleted
+        if (tweet.isDeleted()) {
+            throw new NotFoundException("Tweet with id: " + id + " has been deleted.");
+        }
+        return tweetMapper.entityToDto(tweetRepository.saveAndFlush(tweet));
+    }
+
 }
